d8c32b28072b8c6a5fbca8c84e2668a2c1c5ba9e
gb124121 <gageball@hotmail.com>
2022-04-23 15:11:03 -0600

    HW14

--- HW13/main.js
+++ HW13/main.js
@@ -43,34 +43,33 @@ function preload() {
 }
 
 function create() {
-    //  A simple background for our game
+   
     this.add.image(400, 300, 'sky');
 
 
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
+    
     platforms = this.physics.add.staticGroup();
 
-    //  Here we create the ground.
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
     apples = this.add.sprite(-150, 300, 'apple');
 
-    //  Now let's create some ledges
+    
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
     platforms.create(50, 450, 'ground' );
 
-    // The player and its settings
+    
     player = this.physics.add.sprite(200, 450, 'dude');
 
-    //  Player physics properties. Give the little guy a slight bounce.
+    
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
-    //  Our player animations, turning, walking left and walking right.
+    
     this.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('dude', {
@@ -94,16 +93,16 @@ function create() {
         key: 'right',
         frames: this.anims.generateFrameNumbers('dude', {
             start: 5,
-            end: 8
+            end: 9
         }),
         frameRate: 10,
         repeat: -1
     });
 
-    //  Input Events
+    
     cursors = this.input.keyboard.createCursorKeys();
     
-//  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+
     stars = this.physics.add.group({
         key: 'star',
         repeat: 11,
@@ -116,7 +115,7 @@ function create() {
 
     stars.children.iterate(function (child) {
 
-        //  Give each star a slightly different bounce
+        
         child.setBounceY(Phaser.Math.FloatBetween(0.100,1));
         
 
@@ -125,18 +124,18 @@ function create() {
     bombs = this.physics.add.group();
     
 
-    //  The score
+    
     scoreText = this.add.text(16, 16, 'score: 0', {
         fontSize: '32px',
         fill: '#000'
     });
 
-    //  Collide the player and the stars with the platforms
+    
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(stars, platforms);
     this.physics.add.collider(bombs, platforms);
 
-    //  Checks to see if the player overlaps with any of the stars if he does call the collectStar function
+    
     
     this.physics.add.overlap(player, stars, collectStar, null, this);
 
@@ -181,12 +180,12 @@ function collectStar(player, star) {
 
     
 
-    //  Add and update the score
+    
     score += 10;
     scoreText.setText('Score: ' + score);
 
     if (stars.countActive(true) === 0) {
-        //  A new batch of stars to collect
+        
         stars.children.iterate(function (child) {
             
 
---
+++ HW14/js/main.js
@@ -0,0 +1,151 @@
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var shape, shape2;
+var modelObject;
+// create the first box
+function createBox() {
+  // create a box
+  var geometry = new THREE.BoxGeometry();
+  var material = new THREE.MeshBasicMaterial({
+    color: 0x9D70F9
+  });
+  shape = new THREE.Mesh(geometry, material);
+  shape.position.set(50, 10, 10);
+  scene.add(shape);
+  shape.scale.x = 25; // SCALE
+  shape.scale.y = 25; // SCALE
+  shape.scale.z = 25; // SCALE
+
+
+  animate();
+}
+
+// animate the first box
+function animate() {
+  requestAnimationFrame(animate);
+  shape.rotation.x += 0.15;
+  shape.rotation.y += 0.15;
+  createBox2();
+  renderer.render(scene, camera);
+
+}
+
+
+// create the second box and add it as a child of the first box
+function createBox2() {
+  // create a box
+  var geometry = new THREE.BoxGeometry();
+  var material = new THREE.MeshBasicMaterial({
+    color: 0x02163B
+  });
+  shape2 = new THREE.Mesh(geometry, material);
+  shape2.position.set(2, 5);
+  shape.add(shape2);
+  shape2.scale.x = 1; 
+  shape2.scale.y = .15; 
+  shape2.scale.z = .15; 
+
+  animate2();
+}
+
+
+function animate2() {
+  requestAnimationFrame(animate2);
+  shape2.rotation.x += 0.15;
+  shape2.rotation.y += 0.15;
+
+
+}
+
+
+function getScene() {
+  var scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x95874);
+  return scene;
+}
+
+
+function getCamera() {
+  var aspectRatio = window.innerWidth / window.innerHeight;
+  var camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+  camera.position.set(400, 90, 200);
+  return camera;
+}
+
+/**
+ @param {obj} scene: 
+ **/
+
+function getLight(scene) {
+  var light = new THREE.PointLight(0xffffff, 1, 0);
+  light.position.set(20, 50, 20);
+  scene.add(light);
+
+  var ambientLight = new THREE.AmbientLight(0x111111);
+  scene.add(ambientLight);
+  return light;
+}
+
+
+function getRenderer() {
+  // Create the canvas with a renderer
+  var renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+  
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  
+  document.body.appendChild(renderer.domElement);
+  return renderer;
+}
+
+/**
+ * 
+ * @param {obj} camera: 
+ * @param {obj} renderer: 
+ **/
+
+function getControls(camera, renderer) {
+  var controls = new THREE.TrackballControls(camera, renderer.domElement);
+  controls.zoomSpeed = 0.4;
+  controls.panSpeed = 0.4;
+  return controls;
+}
+
+
+function loadModel() {
+  loader = new THREE.OBJLoader();
+  loader.load('models/Skull.obj', function (object) {
+    object.rotation.z = Math.PI;
+    modelObject = object;
+    scene.add(object);
+    animateModel();
+  });
+}
+
+function animateModel() {
+  requestAnimationFrame(animateModel);
+  modelObject.rotation.x += 0.100;
+  modelObject.rotation.y += 0.100;
+}
+
+
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+  controls.update();
+};
+
+var scene = getScene();
+var camera = getCamera();
+var light = getLight(scene);
+var renderer = getRenderer();
+var controls = getControls(camera, renderer);
+var game1 = createBox();
+
+
+loadModel()
+
+render();
